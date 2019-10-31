0af500f9c908f79520591a1d6a0cd66dff56faa7
charles <codelyfewithcharlie@gmail.com>
2019-10-31 09:05:43 -0700

    updates

--- src/components/StoreGrid/StoreGrid.jsx
+++ src/components/StoreGrid/StoreGrid.jsx
@@ -4,12 +4,79 @@ import "./StoreGrid.scss";
 export default function StoreGrid() {
 	return (
 		<div className='store-grid'>
-			<div className='store-item'>Item 1</div>
-			<div className='store-item'>Item 2</div>
-			<div className='store-item'>Item 3</div>
-			<div className='store-item'>Item 4</div>
-			<div className='store-item'>Item 5</div>
-			<div className='store-item'>Item 6</div>
+			<div className='store-item'>
+				<i class='fas fa-heart'></i>
+				<img
+					className='store-item-img'
+					src='https://www.tennisexpress.com/prodimages/84647-DEFAULT-l.jpg'
+					alt='menswhitetee'
+				/>
+				<div className='store-item-info'>
+					<h2>Dont buy this, its cheap material</h2>
+					<h3>450.98</h3>
+				</div>
+			</div>
+
+			<div className='store-item'>
+				<i class='fas fa-heart'></i>
+				<img
+					className='store-item-img'
+					src='https://www.tennisexpress.com/prodimages/87182-DEFAULT-l.jpg'
+					alt='menswhitetee'
+				/>
+				<div className='store-item-info'>
+					<h2>Dont buy this, its cheap material</h2>
+					<h3>450.98</h3>
+				</div>
+			</div>
+			<div className='store-item'>
+				<i class='fas fa-heart'></i>
+				<img
+					className='store-item-img'
+					src='https://www.tennisexpress.com/prodimages/87323-DEFAULT-l.jpg'
+					alt='menswhitetee'
+				/>
+				<div className='store-item-info'>
+					<h2>Dont buy this, its cheap material</h2>
+					<h3>450.98</h3>
+				</div>
+			</div>
+			<div className='store-item'>
+				<i class='fas fa-heart'></i>
+				<img
+					className='store-item-img'
+					src='https://www.tennisexpress.com/prodimages/83448-DEFAULT-l.jpg'
+					alt='menswhitetee'
+				/>
+				<div className='store-item-info'>
+					<h2>Dont buy this, its cheap material</h2>
+					<h3>450.98</h3>
+				</div>
+			</div>
+			<div className='store-item'>
+				<i class='fas fa-heart'></i>
+				<img
+					className='store-item-img'
+					src='https://www.tennisexpress.com/prodimages/77100-DEFAULT-l.jpg'
+					alt='menswhitetee'
+				/>
+				<div className='store-item-info'>
+					<h2>Dont buy this, its cheap material</h2>
+					<h3>450.98</h3>
+				</div>
+			</div>
+			<div className='store-item'>
+				<i class='fas fa-heart'></i>
+				<img
+					className='store-item-img'
+					src='https://www.tennisexpress.com/prodimages/77097-DEFAULT-l.jpg'
+					alt='menswhitetee'
+				/>
+				<div className='store-item-info'>
+					<h2>Dont buy this, its cheap material</h2>
+					<h3>450.98</h3>
+				</div>
+			</div>
 		</div>
 	);
 }
