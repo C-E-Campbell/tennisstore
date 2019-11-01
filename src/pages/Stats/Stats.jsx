39e6f05cfffe0d0805cae4f7b148e7c9ebe2bddf
charles <codelyfewithcharlie@gmail.com>
2019-11-01 15:20:54 -0700

    updates

--- src/pages/Stats/Stats.jsx
+++ src/pages/Stats/Stats.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+
 import "./Stats.scss";
 import StatItem from "../../components/StatItem/StatItem";
 import { Link } from "react-router-dom";
@@ -9,12 +10,15 @@ export default class Stats extends Component {
 		this.state = {
 			topWomen: [],
 			topMen: [],
-			doublesRank: [],
+			topDoublesMen: [],
+			topDoublesWomen: [],
 			tourneySchedule: [],
 			showWomen: false,
 			showMen: false,
-			showDoubles: false,
-			showTourney: false
+			showDoublesMen: false,
+			showDoublesWomen: false,
+			showTourney: false,
+			loading: true
 		};
 	}
 
@@ -22,10 +26,19 @@ export default class Stats extends Component {
 		const Top20 = await axios.get(
 			"https://cors-anywhere.herokuapp.com/http://api.sportradar.us/tennis-t2/en/players/race_rankings.json?api_key=29n49vy68mxscutdge8ykpax"
 		);
+		const Top20Doubles = await axios.get(
+			"https://cors-anywhere.herokuapp.com/http://api.sportradar.us/tennis-t2/en/double_teams/rankings.json?api_key=29n49vy68mxscutdge8ykpax"
+		);
 		this.setState({
 			topWomen: Top20.data.rankings[0].player_rankings.slice(0, 20),
 			topMen: Top20.data.rankings[1].player_rankings.slice(0, 20),
-			showWomen: true
+			topDoublesMen: Top20Doubles.data.rankings[1].player_rankings.slice(0, 20),
+			topDoublesWomen: Top20Doubles.data.rankings[0].player_rankings.slice(
+				0,
+				20
+			),
+			showWomen: true,
+			loading: false
 		});
 	}
 
@@ -38,7 +51,7 @@ export default class Stats extends Component {
 
 	getDoubles = async () => {
 		const doubleResults = await axios.get(
-			"https://cors-anywhere.herokuapp.com/http://api.sportradar.us/tennis-t2/en/tournaments/sr:tournament:2555/schedule.json?api_key=29n49vy68mxscutdge8ykpax"
+			"https://cors-anywhere.herokuapp.com/http://api.sportradar.us/tennis-t2/en/double_teams/rankings.json?api_key=29n49vy68mxscutdge8ykpax"
 		);
 		console.log(doubleResults.data);
 	};
@@ -47,7 +60,7 @@ export default class Stats extends Component {
 		const mappedWomens = this.state.topWomen.map(player => {
 			return (
 				<StatItem
-					key={player.player.rank}
+					key={player.player.name}
 					name={player.player.name}
 					rank={player.rank}
 					played={player.tournaments_played}
@@ -59,7 +72,7 @@ export default class Stats extends Component {
 		const mappedMens = this.state.topMen.map(player => {
 			return (
 				<StatItem
-					key={player.player.rank}
+					key={player.player.name}
 					name={player.player.name}
 					rank={player.rank}
 					played={player.tournaments_played}
@@ -68,6 +81,31 @@ export default class Stats extends Component {
 				/>
 			);
 		});
+		const mappedDoublesMens = this.state.topDoublesMen.map(player => {
+			return (
+				<StatItem
+					key={player.player.name}
+					name={player.player.name}
+					rank={player.rank}
+					played={player.tournaments_played}
+					points={player.points}
+					nationality={player.player.nationality}
+				/>
+			);
+		});
+		const mappedDoublesWomens = this.state.topDoublesWomen.map(player => {
+			return (
+				<StatItem
+					key={player.player.name}
+					name={player.player.name}
+					rank={player.rank}
+					played={player.tournaments_played}
+					points={player.points}
+					nationality={player.player.nationality}
+				/>
+			);
+		});
+
 		return (
 			<div className='stats-page'>
 				<header className='stats-header'>
@@ -76,26 +114,75 @@ export default class Stats extends Component {
 				<div className='stats-container'>
 					<div className='stat-links'>
 						<h3>Stats and Matches</h3>
-						<h4>Top 20 Womens</h4>
-						<h4>Top 20 Mens</h4>
 						<h4
 							onClick={() => {
-								this.getDoubles();
+								this.setState({
+									showWomen: true,
+									showMen: false,
+									showDoublesMen: false,
+									showDoublesWomen: false,
+									showTourney: false
+								});
 							}}
 						>
-							Doubles Rankings
+							Top 20 Womens
 						</h4>
 						<h4
 							onClick={() => {
-								this.getTourney();
+								this.setState({
+									showWomen: false,
+									showMen: true,
+									showDoublesMen: false,
+									showDoublesWomen: false,
+									showTourney: false
+								});
 							}}
 						>
-							Tournament Schedule
+							Top 20 Mens
 						</h4>
+						<h4
+							onClick={() => {
+								this.setState({
+									showWomen: false,
+									showMen: false,
+									showDoublesMen: false,
+									showDoublesWomen: true,
+									showTourney: false
+								});
+							}}
+						>
+							Doubles Rankings Women
+						</h4>
+						<h4
+							onClick={() => {
+								this.setState({
+									showWomen: false,
+									showMen: false,
+									showDoublesMen: true,
+									showDoublesWomen: false,
+									showTourney: false
+								});
+							}}
+						>
+							Doubles Rankings Men
+						</h4>
+						<h4>Tournament Schedule</h4>
 					</div>
-					<div className='stat-data'>
-						{this.state.showWomen ? mappedWomens : null}
-					</div>
+					{this.state.loading ? (
+						<div className='ui segment myLoader'>
+							<div className='ui active inverted dimmer'>
+								<div className='ui text loader'>Grabbing Tennis Stats</div>
+							</div>
+							<p></p>
+						</div>
+					) : (
+						<div className='stat-data'>
+							{this.state.showWomen ? mappedWomens : null}
+							{this.state.showMen ? mappedMens : null}
+							{this.state.showDoublesMen ? mappedDoublesMens : null}
+							{this.state.showDoublesWomen ? mappedDoublesWomens : null}
+						</div>
+					)}
 				</div>
 			</div>
 		);
