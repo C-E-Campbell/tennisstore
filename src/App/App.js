0d7a2a284a542eb58675044bb2e268a0536aec31
charles <codelyfewithcharlie@gmail.com>
2019-11-05 14:42:50 -0700

    add mens clothing

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./App.scss";
 import { Switch, Route } from "react-router-dom";
-
+import MensShop from "../pages/MensShop/MensShop";
+import MensClothing from "../pages/MensClothing/MensClothing";
 import Home from "../pages/Home/Home";
 import Register from "../pages/Register/Register";
 import Login from "../pages/Login/Login";
@@ -16,6 +17,8 @@ function App() {
 			<Route component={Login} path={"/signin"} />
 			<Route component={Stats} path={"/stats"} />
 			<Route component={WomensShop} path={"/womens"} />
+			<Route component={MensShop} path={"/mens"} />
+			<Route component={MensClothing} path={"/mensclothing"} />
 			<Route component={WomensShop} path={"/womensshoes"} />
 			<Route component={WomensClothing} path={"/womensclothing"} />
 		</Switch>
