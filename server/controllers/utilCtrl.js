b62d25efe336a8e73514f1bde2f335ba32cbf95d
charles <codelyfewithcharlie@gmail.com>
2019-11-10 19:34:03 -0700

    updatessssss

--- server/controllers/utilCtrl.js
+++ server/controllers/utilCtrl.js
@@ -1,4 +1,5 @@
-const stripe = require("stripe")("sk_test_V6asri72BsmkEmXPaPa31vXa00ug3exYPX");
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE);
 const nodemailer = require("nodemailer");
 module.exports = {
 	sendPayment: async (req, res) => {
@@ -20,29 +21,20 @@ module.exports = {
 	sendDiscount: async (req, res) => {
 		const { sendTo } = req.body;
 
-		// let transporter = nodemailer.createTransport({
-		//     host: 'smtp-relay.gmail.com',
-		//     port: 465,
-		//     secure: true, // true for 465, false for other ports
-		//     auth: {
-		//         user: 'codelyfeassets@gmail.com', // generated ethereal user
-		//         pass: 'rufus0606' // generated ethereal password
-		//     }
-		// });
 		let transporter = nodemailer.createTransport({
 			service: "Gmail",
 			auth: {
-				user: "codelyfeassets@gmail.com", // generated ethereal user
-				pass: "rufus0606" // generated ethereal password
+				user: process.env.NodeEmail,
+				pass: process.env.NodePass
 			}
 		});
 
 		await transporter.sendMail({
-			from: "codelyfeassets@gmail.com", // sender address
-			to: sendTo, // list of receivers
-			subject: "Your DevTennis Discount", // Subject line
-			text: "Hello world?", // plain text body
-			html: "<h2>Use Discount Code: tennisaintfreebiiiiitch</h2>" // html body
+			from: process.env.NodeEmail,
+			to: sendTo,
+			subject: "Your DevTennis Discount",
+			text: "Hello",
+			html: "<h2 >Use Discount Code: 15%_MoreHappy!</h2>"
 		});
 	}
 };
